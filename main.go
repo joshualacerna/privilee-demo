64b4404a41292b8efc2b485d2aff2f9d4df425d6
Joshua Lacerna <joshua.lacerna@mondia.com>
2024-04-08 12:20:28 +0400

    Adds script

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os/exec"
 
 	"github.com/labstack/echo/v4"
 )
@@ -9,7 +11,15 @@ import (
 func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, Privilee!!!")
+		out, err := exec.Command("/bin/sh", "zone.sh").Output()
+		// return fmt.Println(string(output))
+		if err != nil {
+			log.Fatal(err)
+		}
+		if len(out) == 0 {
+			log.Fatal("no output was returned from script")
+		}
+		return c.String(http.StatusOK, "Hello, Privilee!!! \nfrom region "+string(out))
 	})
 	e.Logger.Fatal(e.Start(":1323"))
 }
